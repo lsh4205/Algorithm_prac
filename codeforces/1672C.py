b074153afe3b9ff4b4d9ff4eb3ac1adf3a7a2735
Lee Byron Seonghyun <lsh42059907@gmail.com>
2022-05-25 14:31:27 +0000

    0 -> -1

--- codeforces/1672C.py
+++ codeforces/1672C.py
@@ -4,10 +4,10 @@ s = int(input())
 for i in range(s):
 	size = int(input())
 	arr = list(map(int, input().split()))
-	l, r = 0, -1
+	l, r = -1, -1
 	for i in range(1, size):
 		if arr[i] == arr[i-1]:
-			if l == 0:
+			if l == -1:
 				l = i
 			r = i
 	if l == r:
