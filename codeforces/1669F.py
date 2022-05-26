caa7de49c3957c71ffacf0f5387489798cb2b6ff
Lee Byron Seonghyun <lsh42059907@gmail.com>
2022-05-26 05:35:08 +0000

    Change method

--- codeforces/1669F.py
+++ codeforces/1669F.py
@@ -1,25 +1,24 @@
 import sys
 
 s = int(input())
-print(s)
+
 for i in range(s):
     size = int(input())
     arr = list(map(int, input().split()))
     
-    sum = 0
-    for i in range(len(arr)):
-        sum += arr[i]
+    l, r = arr[0], arr[size-1]
+    l_p, r_p = 1, size-2
     
-    print(sum)
-        
-    if sum%2 == 1:
-        print(0)
-    else:
-        equal = int(sum/2)
-        count =0
-        for i in range(size):
-            if equal == 0:
-                print(count)
+    while l_p < size-2 and r_p > 1:
+        if l < r:
+            l += arr[l_p+1]
+            l_p += 1
+        else:
+            r += arr[r_p-1]
+            r_p -= 1
+    print(f'{l_p}, {r_p}')       
+    if l_p >= r_p:
+            if l == r:
+                print(l_p-0 + size-1-r_p)
             else:
-                equal -= arr[i]
-                count += 1
+                print(0)
