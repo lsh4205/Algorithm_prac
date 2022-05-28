a86aeec133b8b4dcc31fc97fb9ef798b1deb84c1
Lee Byron Seonghyun <lsh42059907@gmail.com>
2022-05-28 07:52:03 +0000

    Fixed initial value of l_p and r_p, due to failure of test_2 on codeforces

--- codeforces/1669F.py
+++ codeforces/1669F.py
@@ -3,28 +3,20 @@ import sys
 s = int(input())
 
 for i in range(s):
-    size = int(input())
+    n = int(input())
     arr = list(map(int, input().split()))
     
-    l, r = arr[0], arr[size-1]
-    l_p, r_p = 1, size-2
-    last_l, last_r = -1, -1
+    l_p, r_p = 0, n-1
+    l, r, cout = 0,0,0
     
     while l_p <= r_p:
-        if l == r:
-            last_l = l_p
-            last_r = r_p
         if l < r:
             l += arr[l_p]
             l_p += 1
         else:
             r += arr[r_p]
-            r_p -= 1     
+            r_p -= 1 
+        if l == r:
+            cout = l_p + (n - r_p) -1
+    print(cout)
 
-    if l == r:
-        print(l_p + size-1 - r_p)
-    else:
-        if last_l != -1:
-            print(last_l + size-1 - last_r)
-        else:
-            print(0)
