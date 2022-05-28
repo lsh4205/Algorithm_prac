d1f94b2d9efd266ab00350232c5ff1ad6b88ab58
Lee Byron Seonghyun <lsh42059907@gmail.com>
2022-05-28 07:41:39 +0000

    Final edit of 1669F

--- codeforces/1669F.py
+++ codeforces/1669F.py
@@ -10,22 +10,21 @@ for i in range(s):
     l_p, r_p = 1, size-2
     last_l, last_r = -1, -1
     
-    while l_p < size-2 and r_p > 1:
-        if l < r:
-            l += arr[l_p+1]
-            l_p += 1
-        elif r < l:
-            r += arr[r_p-1]
-            r_p -= 1
-        else:
+    while l_p <= r_p:
+        if l == r:
             last_l = l_p
             last_r = r_p
+        if l < r:
+            l += arr[l_p]
             l_p += 1
-            r_p -= 1 
-            
-    print(f'{l_p}, {r_p}')       
-    if l_p >= r_p:
-            if l == r:
-                print(l_p-0 + size-1-r_p)
-            else:
-                print(0)
+        else:
+            r += arr[r_p]
+            r_p -= 1     
+
+    if l == r:
+        print(l_p + size-1 - r_p)
+    else:
+        if last_l != -1:
+            print(last_l + size-1 - last_r)
+        else:
+            print(0)
