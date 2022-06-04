0f1ee4bfa16812373d96febef069cbe0ac3ab89e
Lee Byron Seonghyun <lsh42059907@gmail.com>
2022-06-04 13:44:31 +0000

    .

--- codeforces/1676F.py
+++ codeforces/1676F.py
@@ -6,24 +6,27 @@ for x in range(s):
     f = list(map(int, input().split()))
     arr = list(map(int, input().split()))
     
-    arr = arr.sort()
+    arr.sort()
+    print(arr)
     n, k = f[0], f[1]
-    l, l_v, r, r_v, cout = 0, arr[0], 0, arr[1], 1
-    strike = Tru
+    l_v, r_v = arr[0], arr[0]
+    l, r, cout = 0, 0, 0
+    strike = False
         
-    for i in range(n):
-        if cout >= k:
-            l_v, r_v = min(l_v,arr[l]), max(r_v,arr[r])
-            strike = True 
-            
-        r = l+1
+    while r < n:
         if arr[l] == arr[r]:
-            strike = True
+            print(f'count={cout}')
             cout += 1
+            print(f'at {l} {arr[l]} == at {r} {arr[r]}, now count={cout}')
+            if cout >= k:
+                strike = True
+                l_v, r_v = min(l_v,arr[l]), max(r_v,arr[r])
+                l = r
+            r += 1
         else:
-            strike = False
-            cout = 1
+            cout = 0
             l = r
+            print(f'at {l} {arr[l]} != at {r} {arr[r]}, now count={cout}')
             
     if strike:
         print(f'{l_v} {r_v}')
