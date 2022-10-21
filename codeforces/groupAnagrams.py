bb8074f397235d7d3e27b5536cd0a612baa358de
lsh4205 <63761734+lsh4205@users.noreply.github.com>
2022-10-21 00:12:08 -0400

    New problem 'group Anagrams'

---
+++ codeforces/groupAnagrams.py
@@ -0,0 +1,31 @@
+# Given an array of strings strs, 
+# group the anagrams together. 
+# You can return the answer in any order.
+
+# An Anagram is a word or phrase formed
+# by rearranging the letters of a different word or phrase,
+# typically using all the original letters exactly once.
+
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+
+def groupAnagrams(strs):
+    # first thought : using dictionary
+    final_result = []
+    hash_idx = set()
+    for i, str in enumerate(strs):
+        if i in hash_idx:
+            continue
+        chars = set(str)
+        result = []
+        for j, str2 in enumerate(strs):
+            chars2 = set(str2)
+            if chars2 != chars:
+                continue
+            else:
+                hash_idx.add(j)
+                result.append(str2)
+        final_result.append(result)
+strs = ["eat","tea","tan","ate","nat","bat"]
+groupAnagrams(strs)
